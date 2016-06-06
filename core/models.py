5a95c67d82a921dcb64782b882b66af25a10b832
Arnold0824 <Schwarzenegger@vip.qq.com>
2016-06-07 00:05:06 +0800

    ss

--- core/models.py
+++ core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
-class users(models)
+class users(models.Model):
+    clientid=models.CharField('微信用户openid',max_length=50,null=False,blank=True)
 
 # Create your models here.
--- core/views.py
+++ core/views.py
@@ -1,3 +1,48 @@
 from django.shortcuts import render
+#from core.models import TASK,USER,CPN,CLM,CLMLST
+from core.models import *
+from django.http import HttpResponse, HttpResponseRedirect
+#from PIL import Image,ImageDraw,ImageFont
+from math import ceil
+import random
+import os
+import io as cStringIO
+from datetime import datetime
+import hashlib
+from django.core.files.storage import default_storage
+import json
+import time
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db import connection
+from django.utils import timezone
+try:
+    from functools import wraps
+except ImportError:
+    from django.utils.functional import wraps  # Python 2.4 fallback.
+from django.utils.decorators import available_attrs
+import base64
+from pyDes import *
+import urllib.parse
+import urllib
+import sys
+import http.cookiejar
+import json
 
-# Create your views here.
+
+def my_custom_sql(sql,*para):
+    cursor = connection.cursor()
+
+    cursor.execute(sql,*para)
+
+    #cursor.execute("SELECT foo FROM bar WHERE baz = %s", [self.baz])
+    row = cursor.fetchall()
+
+    return row
+def sign(s):#签名加密方式
+    k = des("mv03nd.f", ecb, "\0\0\0\0\0\0\0\0",
+                pad=none, padmode=pad_pkcs5)
+    return base64.b64encode(k.encrypt(s))
+def post(url, data):#封装post方法
+    return urllib.request.urlopen(url, urllib.parse.urlencode(data).encode('utf-8')).read()
+
+
